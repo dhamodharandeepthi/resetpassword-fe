cf2140e21ed485ad1d6bca435183e8464f8f9024
Dhamodharandeepthi <rdhamodharan22@gmail.com>
2024-01-12 16:26:23 +0530

    render added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from './pages/Signup';
 import NoPage from './pages/NoPage';
+import Forgot from './pages/Forgot';
+import NewPassword from './pages/NewPassword';
 
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Login />} />
         <Route exact path="/register" element={<Signup />} />
+        <Route exact path="/forgot" element={<Forgot />} />
+        <Route exact path="/reset/:id/:token" element={<NewPassword />} />
         <Route exact path="/*" element={<NoPage />} />
       </Routes>
     </>
--- src/helpers/API.js
+++ src/helpers/API.js
@@ -1,5 +1,5 @@
 // // Server side URL
-export const API = "https://reset-password-server-qwgy.onrender.com";
+export const API = "https://reset-be.onrender.com";
 
  // client side URL for new password
-// export const AppLink =
+// export const AppLink = 
